34a283a2c711c2770f3630dbd4f851a0a4d42205
muyeby <bxf_hit@163.com>
2018-03-08 20:59:25 +0800

    update mygan

--- GAN/MyGAN.py
+++ GAN/MyGAN.py
@@ -21,6 +21,7 @@ from embedding import WordEmbeddings
 from sklearn.preprocessing import normalize
 from sklearn.utils import check_random_state
 
+torch.cuda.set_device(1)
 
 g_input_size = 50     # Random noise dimension coming into generator, per output vector
 g_hidden_size = 50   # Generator complexity
@@ -32,10 +33,13 @@ d_output_size = 1    # Single dimension for 'real' vs. 'fake'
 
 HALF_BATCH_SIZE = 128
 
-d_learning_rate = 2e-4  # 2e-4
-g_learning_rate = 2e-4
+# d_learning_rate = 2e-4  # 2e-4
+d_learning_rate = 0.001
+# g_learning_rate = 2e-4
+g_learning_rate = 0.001
+
 optim_betas = (0.9, 0.999)
-num_epochs = 10000000
+num_epochs = 500000
 print_interval = 100
 d_steps = 1  # 'k' steps in the original GAN paper. Can put the discriminator on higher training freq than generator
 g_steps = 1
@@ -45,7 +49,8 @@ hidden_noise = 0.5
 
 gloss_min = 100000
 
-torch.cuda.set_device(1)
+TrainNew = True
+
 
 def get_distribution_sampler(mu, sigma):
     return lambda n: torch.Tensor(np.random.normal(mu, sigma, (n, d)))  # Gaussian
@@ -106,87 +111,98 @@ rng = check_random_state(0)
 
 
 we1 = WordEmbeddings()
-we1.load_from_word2vec(dataDir, 'en')
+we1.load_from_word2vec(dataDir, 'zh')
 we1.downsample_frequent_words()
 we1.vectors = normalize(we1.vectors)
 we_batches1 = we1.sample_batches(batch_size=HALF_BATCH_SIZE, random_state=rng)
 
 we2 = WordEmbeddings()
-we2.load_from_word2vec(dataDir, 'zh')
+we2.load_from_word2vec(dataDir, 'en')
 we2.downsample_frequent_words()
 we2.vectors = normalize(we2.vectors)
 we_batches2 = we2.sample_batches(batch_size=HALF_BATCH_SIZE, random_state=rng)
 
 assert we1.embedding_dim == we2.embedding_dim
 d = we1.embedding_dim
+if TrainNew:
+    for epoch in range(num_epochs):
 
-for epoch in range(num_epochs):
+        for d_index in range(d_steps):
+            # 1. Train D on real+fake
+            D.zero_grad()
+            id1 = next(we_batches1)
+            id2 = next(we_batches2)
 
-    for d_index in range(d_steps):
-        # 1. Train D on real+fake
-        D.zero_grad()
-        id1 = next(we_batches1)
-        id2 = next(we_batches2)
+            d_input_data = Variable(torch.from_numpy(we1.vectors[id1]))
+            d_trg_data = Variable(torch.from_numpy(we2.vectors[id2]))
 
-        d_input_data = Variable(torch.from_numpy(we1.vectors[id1]))
-        d_trg_data = Variable(torch.from_numpy(we2.vectors[id2]))
+            #  1A: Train D on real
 
-        #  1A: Train D on real
+            d_real_decision = D(d_trg_data.cuda().float())
+            d_real_error = criterion(d_real_decision, Variable(torch.ones(HALF_BATCH_SIZE)).cuda())  # ones = true
+            d_real_error.backward() # compute/store gradients, but don't change params
 
-        d_real_decision = D(d_trg_data.cuda().float())
-        d_real_error = criterion(d_real_decision, Variable(torch.ones(HALF_BATCH_SIZE)).cuda())  # ones = true
-        d_real_error.backward() # compute/store gradients, but don't change params
+            #  1B: Train D on fake
+            d_sampler = get_distribution_sampler(1, input_noise)
+            gaussian_noise = d_sampler(HALF_BATCH_SIZE)
 
-        #  1B: Train D on fake
-        d_sampler = get_distribution_sampler(1, input_noise)
-        gaussian_noise = d_sampler(HALF_BATCH_SIZE)
+            d_fake_data = G(d_input_data.cuda().float()).detach()  # detach to avoid training G on these labels,假设G固定
 
-        d_fake_data = G(d_input_data.cuda().float()).detach()  # detach to avoid training G on these labels,假设G固定
-
-        tmp = d_fake_data.data
-        # print(tmp)
-        # print(gaussian_noise)
-        d_fake_input = Variable(tmp * gaussian_noise.cuda())
-        d_fake_decision = D(d_fake_input.cuda().float())
-        d_fake_error = criterion(d_fake_decision, Variable(torch.zeros(HALF_BATCH_SIZE)).cuda())  # zeros = fake
-        d_fake_error.backward()
-        d_optimizer.step()     # Only optimizes D's parameters; changes based on stored gradients from backward()
-
-    for g_index in range(g_steps):
-        # 2. Train G on D's response (but DO NOT train D on these labels)
-        G.zero_grad()
-
-        id1 = next(we_batches1)
-        id2 = next(we_batches2)
-
-        d_input_data = Variable(torch.from_numpy(we1.vectors[id1]))
-        d_trg_data = Variable(torch.from_numpy(we2.vectors[id2]))
-
-        g_fake_data = G(d_input_data.cuda().float())
-        dg_fake_decision = D(g_fake_data)
-        g_error = criterion(dg_fake_decision, Variable(torch.ones(HALF_BATCH_SIZE)).cuda())  # we want to fool, so pretend it's all genuine
-        g_res_loss = 1.0 - torch.mean(criterion2(d_input_data.cuda().float(),g_fake_data))
-
-        loss = g_error+ g_res_loss
-        loss.backward()
-        # g_error.backward()
-        # g_res_loss.backward()
-        g_optimizer.step()  # Only optimizes G's parameters
-
-    if epoch % print_interval == 0:
-        print("step: {}  rec_loss:{}  V:{}".format(epoch, g_res_loss.data[0], g_error.data[0]))
-
-    if (epoch> 10000) and (g_error.data[0] < gloss_min):
-        gloss_min = g_error.data[0]
-        W = G.map.weight.data.cpu().numpy()
-        torch.save(G.state_dict(), 'g_params.pkl')
-        print("recon_gen_loss_val:{}  ||W^T*W - I||:{}".format(g_res_loss.data[0],
-                                                               np.linalg.norm(np.dot(W.T, W) - np.identity(d))))
+            tmp = d_fake_data.data
+            # print(tmp)
+            # print(gaussian_noise)
+            d_fake_input = Variable(tmp * gaussian_noise.cuda())
+            d_fake_decision = D(d_fake_input.cuda().float())
+            d_fake_error = criterion(d_fake_decision, Variable(torch.zeros(HALF_BATCH_SIZE)).cuda())  # zeros = fake
+            d_fake_error.backward()
+            d_optimizer.step()     # Only optimizes D's parameters; changes based on stored gradients from backward()
 
 
-G2 = torch.load('g_params.pkl')
-W = G2.map.weight.data.cpu().numpy()
+        for g_index in range(g_steps):
+            # 2. Train G on D's response (but DO NOT train D on these labels)
+            G.zero_grad()
 
-we1.transformed_vectors = np.dot(we1.vectors, W)
+            id1 = next(we_batches1)
+            id2 = next(we_batches2)
+
+            d_input_data = Variable(torch.from_numpy(we1.vectors[id1]))
+            d_trg_data = Variable(torch.from_numpy(we2.vectors[id2]))
+
+            g_fake_data = G(d_input_data.cuda().float())
+
+            g_recon_data = torch.mm(g_fake_data,G.map.weight.t())
+
+            dg_fake_decision = D(g_fake_data)
+
+            g_error = criterion(dg_fake_decision, Variable(torch.ones(HALF_BATCH_SIZE)).cuda())  # we want to fool, so pretend it's all genuine
+            g_recon_loss = 1.0 - torch.mean(criterion2(d_input_data.cuda().float(),g_recon_data))
+
+            loss = g_error+ g_recon_loss
+            loss.backward()
+            # g_error.backward()
+            # g_res_loss.backward()
+            g_optimizer.step()  # Only optimizes G's parameters
+
+        # if epoch % print_interval == 0:
+        #     print("step: {}  rec_loss:{}  V:{}".format(epoch, g_res_loss.data[0], g_error.data[0]))
+
+        if (epoch> 10000) and (loss.data[0] < gloss_min):
+            gloss_min = loss.data[0]
+            W = G.map.weight.data.cpu().numpy()
+            torch.save(G.state_dict(), 'g_params.pkl')
+            print("epoch:{} sum_loss:{}".format(epoch,loss.data[0]))
+            print(" recon_gen_loss_val:{}  ||W^T*W - I||:{}".format(g_recon_loss.data[0],
+                                                                   np.linalg.norm(np.dot(W.T, W) - np.identity(d))))
+
+G2 = Generator(input_size=g_input_size, output_size=g_output_size).cuda()
+G2.load_state_dict(torch.load('g_params.pkl'))
+
+d_input_data_all = Variable(torch.from_numpy(we1.vectors))
+
+transformed_data = G2(d_input_data_all.cuda().float())
+
+we1.transformed_vectors = transformed_data.data.cpu().numpy()
+# W = G2.map.weight.data.cpu().numpy()
+#
+# we1.transformed_vectors = np.dot(we1.vectors, W)
 we1.save_transformed_vectors(dataDir + 'transformed-' + '.' + 'zh')
-
